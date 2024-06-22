98b8aef38849df840effb361ccda4006a974730e
Robert <roberthcjr1999@gmail.com>
2024-06-22 14:03:03 -0300

    fix(ga): changes tournement to return only a winner

--- gaProperties/ga.py
+++ gaProperties/ga.py
@@ -16,47 +16,17 @@ class GA:
         self.population.sort(key=lambda individuo: individuo.fitness())
         self.bestIndividuals = self.population[:self.elitism]
     
-    def newPopulation(self, population):
-        newPopulation = population[:]
-        father:Individual = self.tournament()
-        mother:Individual = self.tournament()
-        while mother == father:
-            father = self.tournament()
+    def tournament(self, competidor1, competidor2):
+        winner = None
+        loser = None
 
-        daughter, son = self.getChild(father, mother)
-
-        newPopulation.append([daughter, son])
-
-        self.population = newPopulation.sort(key=lambda individual:individual.fitness())[:len(self.population)]
-
-    def getChild(self, father:Individual, mother:Individual):
-        child1 = self.crossOver(father, mother)
-        child2 = self.crossOver(mother, father)
-
-        return child1, child2
-
-    def crossOver(self, individualA:Individual, individualB:Individual, genomeLen):
-        crossOverPosition = random.randint(0, genomeLen)
-        return splitDictFoward(individualA.localesIndexes, crossOverPosition) + splitDictBackward(individualB)
-    
-
-    def tournament(self, population):
-        competitor1 = population[random.randint(0, len(population) - 1)]
-        competitor2 = population[random.randint(0, len(population) - 1)]
-
-        while competitor1 == competitor2:
-            competitor2 = population[random.randint(0, population.len() - 1)]
-
-        bestCompetitor = None
-        worstCompetitor = None
-
-        if competitor1.fitness() > competitor2.fitness():
-            bestCompetitor = competitor1
-            worstCompetitor = competitor2
+        if competidor1.fitness() > competidor2.fitness():
+            winner = competidor1
+            loser = competidor2
         else:
-            bestCompetitor = competitor2
-            worstCompetitor = competitor1
+            winner = competidor2
+            loser = competidor1
         
         if random.uniform(0, 1) < self.kRatio:
-            return bestCompetitor
-        return worstCompetitor
+            return winner
+        return loser
