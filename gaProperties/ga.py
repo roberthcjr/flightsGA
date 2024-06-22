3d3e36c346bb182140abae26250e24df5c2b6275
Robert <roberthcjr1999@gmail.com>
2024-06-22 14:02:18 -0300

    feat(ga): defines major ratios and indexes in constructor

--- gaProperties/ga.py
+++ gaProperties/ga.py
@@ -4,9 +4,14 @@ from utils.splitter import splitDictBackward, splitDictFoward
 
 class GA:
 
-    def __init__(self, flightService, kRatio, locales, toRome, size=100):
+    def __init__(self, flightService, kRatio, crossOverRatio, mutationRatio, elitism, locales, toRome, size=100):
         self.flightService = flightService
         self.kRatio = kRatio
+        self.crossOverRatio = crossOverRatio
+        self.mutationRatio = mutationRatio
+        self.toRome = toRome
+        self.size = size
+        self.elitism = elitism
         self.population = [Individual(self.flightService, locales, toRome) for _ in range(size)]
         self.population.sort(key=lambda individuo: individuo.fitness())
         self.bestIndividuals = self.population[:self.elitism]
