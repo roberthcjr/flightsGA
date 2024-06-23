66225fbedd82741b4df82c22cc94d1c5e7741b31
Robert <roberthcjr1999@gmail.com>
2024-06-23 18:43:49 -0300

    feat(output): methods to plot graphs and write csv

--- utils/output.py
+++ utils/output.py
@@ -1,3 +1,6 @@
+import csv
+
+from matplotlib import pyplot as plt
 from utils.translator import fullNameLocales, toHours
 
 def printDepartureAndArriveToRome(flightsService, key, value):
@@ -21,3 +24,27 @@ def printResult(flightsService, bestIndividual):
             printDepartureAndArriveToRome(flightsService, key, value)
         else:
             printDepartureAndArriveFromRome(flightsService, key, value)
+
+def writeCSV(results):
+      with open('eggs.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for result in results:
+            spamwriter.writerow(results)
+
+def plotGraph(results, isPoint=False):
+    if not isPoint:
+        for key in results.keys():
+            x = list(range(1, len(results[key])+1))
+            plt.plot(x, [value.fitness() for value in results[key]], label = key)
+    else:
+        values = list(results.values())
+        print(values[0])
+        print(values[1])
+        plt.figure(figsize=(10, 6))
+        plt.scatter(values[1], values[0], marker='o', color='r', label='Pontos')
+        plt.xlim(0, 500)
+        plt.ylim(0, 4000)
+        plt.grid(True)
+    plt.legend()
+    plt.show()
