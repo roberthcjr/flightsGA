154ab085cc0cf3d5397c2f0b885d057471fa8f3f
Robert <roberthcjr1999@gmail.com>
2024-06-23 23:38:32 -0300

    feat(main): adds statitics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import copy
+import statistics as sts
+import numpy as np
 from utils.flightsService import Flights
 from gaProperties.ga import GA
 from utils.output import printResult, csv, plotGraph
@@ -53,11 +55,31 @@ def main():
     toRome = False
     kRatio = 0.65
     crossOverRatio = 0.8
-    mutationRatio = 0.25
+    mutationRatio = 0.05
     genQuantity = 200
     results = runGa(toRome=toRome, kRatio=kRatio, crossOverRatio=crossOverRatio, mutationRatio=mutationRatio, genQuantity=genQuantity)
     plotGraph(results=results)
     # results = runMultipleGas(quantity=30, toRome=toRome, kRatio=kRatio, crossOverRatio=crossOverRatio, mutationRatio=mutationRatio, genQuantity=genQuantity)
     # plotGraph(results=results, isPoint=True)
+    allResults = []
+    bestResults = []
+    for key in results.keys():
+        if key == "bestResults":
+            bestResults += results[key]
+        allResults += results[key]
+    allFitness = [individuo.fitness() for individuo in allResults]
+    bestFitness = [individuo.fitness() for individuo in bestResults]
+
+    print("Variancia(todos): ", np.var(allFitness))
+    print("Mediana(todos):", np.median(allFitness))
+    print("Média(todos):", np.mean(allFitness))
+    print("Moda(todos):", sts.mode(allFitness))
+    print("Desvio padrão(todos):", np.std(allFitness))
+
+    print("\nVariancia(melhor): ", np.var(bestFitness))
+    print("Mediana(melhor):", np.median(bestFitness))
+    print("Média(melhor):", np.mean(bestFitness))
+    print("Moda(melhor):", sts.mode(bestFitness))
+    print("Desvio padrão(melhor):", np.std(bestFitness))
 if __name__ == "__main__":
     main()
